6de42205fede80b973bbab0c086fd541d2e1d739
David Demers <fonemstr@gmail.com>
2013-09-25 13:06:40 -0400

    fix syntax error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Spree::Core::Engine.routes.prepend do
 
   get 'blog/tag/:tag', :to => 'blog_entries#tag' #, :as => :tag
   get 'blog/:year/:month/:day/:slug', :to => 'blog_entries#show' #, :as => :blog_entry_permalink
-  get 'blog/:year(/:month)(/:day)', :to => 'blog_entries#archive' #, :as => :blog_news_archive,
-    :constraints => {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}
+  get 'blog/:year(/:month)(/:day)', :to => 'blog_entries#archive', :constraints => {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}
 end
