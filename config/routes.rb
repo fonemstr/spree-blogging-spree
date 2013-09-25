b6d213ce914218920eef1d817f70f2082ff28b57
David Demers <fonemstr@gmail.com>
2013-09-25 15:26:41 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Spree::Core::Engine.routes.prepend do
   
   resources :blog_entries
 
-  get 'blog/tag/:tag', :to => 'blog_entries#tag' #, :as => :tag
-  get 'blog/:year/:month/:day/:slug', :to => 'blog_entries#show' #, :as => :blog_entry_permalink
-  get 'blog/:year(/:month)(/:day)', :to => 'blog_entries#archive', :constraints => {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}
+  get 'blog/tag/:tag', to: 'blog_entries#tag', as: :tag
+  get 'blog/:year/:month/:day/:slug', to: 'blog_entries#show', as: :entry_permalink
+  get 'blog/:year(/:month)(/:day)', to: 'blog_entries#archive', constraints: {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}
 end
