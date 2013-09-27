8428b7479936d130626b8f4547ffb9075fc253a2
David Demers <fonemstr@gmail.com>
2013-09-27 06:19:43 -0400

    add news_archive route name back

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Spree::Core::Engine.routes.draw do 
-  get 'blog/tag/:blog_tag', to: 'blog_entries#tag', as: :tag
-  get 'blog/:year/:month/:day/:slug', to: 'blog_entries#show', as: :entry_permalink
-  get 'blog/:year(/:month)(/:day)', to: 'blog_entries#archive', 
-  constraints: {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}, as: :news_archive
-
   resources :blog_entries
-  
   namespace :admin do
     resources :blog_entries
   end 
+  get 'blog/tag/:blog_tag', to: 'blog_entries#tag', as: :tag
+  get 'blog/:year/:month/:day/:slug', to: 'blog_entries#show', as: :entry_permalink
+  get 'blog/:year(/:month)(/:day)', to: 'blog_entries#archive', as: :news_archive, constraints: {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}  
 end
