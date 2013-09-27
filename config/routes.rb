fb90b654c87a1155a38c6e5131c64e15859ebcd8
David Demers <fonemstr@gmail.com>
2013-09-26 21:21:16 -0400

    add news_archive path in bloggin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Spree::Core::Engine.routes.draw do 
   get 'blog/tag/:blog_tag', to: 'blog_entries#tag', as: :tag
   get 'blog/:year/:month/:day/:slug', to: 'blog_entries#show', as: :entry_permalink
-  get 'blog/:year(/:month)(/:day)', to: 'blog_entries#archive', constraints: {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}
+  get 'blog/:year(/:month)(/:day)', to: 'blog_entries#archive', 
+  constraints: {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}, as: :news_archive
 
   resources :blog_entries
   
